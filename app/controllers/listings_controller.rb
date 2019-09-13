25af989f30cd5d4b7e56a74881d5e06ea4373676
timbog80 <timbog80@gmail.com>
2019-09-12 17:04:50 -0700

    initial commit

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,52 @@
+class ListingsController < ApplicationController
+  before_action :find_listing, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @listings = Listing.all.order("created_at DESC")
+  end
+
+  def show
+
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(set_listing)
+    if @listing.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @listing.update(set_listing)
+      redirect_to listing_path(@listing)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @listing.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_listing
+    params.require(:listing).permit(:title, :description)
+  end
+
+  def find_listing
+    @listing = Listing.find(params[:id])
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  
+  root 'listings#index'
+  resources :listings
+end
